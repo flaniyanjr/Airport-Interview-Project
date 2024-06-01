d4b80a04d98ccf50330b5e1b1586b39140b0ac30
Felix Laniyan <flaniyanjr@gmail.com>
2024-05-31 22:35:14 -0400

    order of time validation finished

--- src/components/App.js
+++ src/components/App.js
@@ -35,14 +35,23 @@ function App() {
     })
   }, [])
 
+// Adding the first flgiht into the rotation column
   function addToRotation(newFlight) {
     if (rotationList.length === 0) {
       setRotationList(current => [...current, newFlight])
-    } else if (rotationList.length > 0 && rotationList[rotationList.length-1].destination === newFlight.origin) {
+
+// Validations that lead to a successful entry in the rotation column
+    } else if (rotationList.length > 0 && rotationList[rotationList.length-1].destination === newFlight.origin && rotationList[rotationList.length-1].departuretime < newFlight.departuretime) {
       setRotationList(current => [...current, newFlight])
+
+// Validation for if flight doesn't leave from correct airport
     } else if (rotationList.length > 0 && rotationList[rotationList.length-1].destination !== newFlight.origin) {
-      alert(`Flight's origin must be ${rotationList[rotationList.length-1].destination}`)
-    } 
+      alert(`Error: Flight's origin must be ${rotationList[rotationList.length-1].destination}`)
+
+// Validation for if flight's aren't placed in order of time
+    } else if (rotationList.length > 0 && rotationList[rotationList.length-1].departuretime > newFlight.departuretime) {
+      alert('Error: Flights need to be placed in order of time')
+    }
   }
 
   function removeFromRotation(ident) {
