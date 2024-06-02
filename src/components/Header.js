762df0fdc734a3358576fb98611f056e76301ea0
Felix Laniyan <flaniyanjr@gmail.com>
2024-06-01 22:13:48 -0400

    added column headers

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,18 @@
 
-
 function Header() {
     return(
-        <div className="header">
-            <button>{"<"}</button>
-            <p>May 30th, 2024</p>
-            <button>{">"}</button>
-        </div>
+        <>
+            <div className="header">
+                <button>{"<"}</button>
+                <p>May 30th, 2024</p>
+                <button>{">"}</button>
+            </div>
+            <div className= 'title'>
+                <h3>Aircrafts</h3>
+                <h3>Rotation A320</h3>
+                <h3>Flights</h3>
+            </div>
+        </>
     )
 }
 
--- src/components/RotationCard.js
+++ src/components/RotationCard.js
@@ -10,11 +10,11 @@ function RotationCard({removeFromRotation, rotationObj}) {
     return(
         <div className= 'rotation-box'>
             <div className="flight-box">
-            <p>Flight: {ident}</p>
-            <p>{origin} → {destination}</p>
-            <p>{readable_departure} - {readable_arrival}</p>
-            <button onClick={handleDelete}> delete </button>
-        </div>
+                <p>Flight: {ident}</p>
+                <p>{origin} → {destination}</p>
+                <p>{readable_departure} - {readable_arrival}</p>
+                <button onClick={handleDelete}> delete </button>
+            </div>
         </div>
     )
 }
