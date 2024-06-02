7c7f2cdd4c35145d328b6ce9688e82740937ab47
Felix Laniyan <flaniyanjr@gmail.com>
2024-06-02 13:43:08 -0400

    fixed validations

--- src/components/App.js
+++ src/components/App.js
@@ -33,30 +33,13 @@ function App() {
     })
   }, [])
 
-// Adding the first flight into the rotation column
+
   function addToRotation(newFlight) {
-    if (rotationList.length === 0) {
-      setRotationList(current => [...current, newFlight])
-
-// Validations that lead to a successful entry in the rotation list
-    } else if (rotationList.length > 0 && rotationList[rotationList.length-1].destination === newFlight.origin && rotationList[rotationList.length-1].departuretime < newFlight.departuretime && rotationList[rotationList.length-1].arrivaltime < newFlight.departuretime && (newFlight.departuretime - rotationList[rotationList.length-1].arrivaltime) >= 1200 && newFlight.departuretime >= 60 & newFlight.arrivaltime >= 60) {
-      setRotationList(current => [...current, newFlight])
-
-// Validation for if flight doesn't leave from correct airport
-    } else if (rotationList.length > 0 && rotationList[rotationList.length-1].destination !== newFlight.origin) {
-      alert(`Error: Flight's origin must be ${rotationList[rotationList.length-1].destination}`)
-
-// Validation for if flight's aren't placed in order of time
-    } else if (rotationList.length > 0 && rotationList[rotationList.length-1].departuretime > newFlight.departuretime || rotationList[rotationList.length-1].arrivaltime > newFlight.departuretime) {
-      alert('Error: Flights need to be placed in order of time')
-
-// Validation for if the next flight is less than 20 mins after the previous flight
-    } else if (rotationList.length > 0 && (newFlight.departuretime - rotationList[rotationList.length-1].arrivaltime) < 1200) {
-      alert("Error: Flight's departure time must be at least 20 min after previous flight's arrival time")
-
-// Validation to make sure flights can't depart or arrive at midnight
-    } else if (newFlight.departuretime < 60 || newFlight.arrivaltime < 60) {
+  // Validation to make sure flights can't depart or arrive at midnight
+    if (newFlight.departuretime < 60 || newFlight.arrivaltime < 60) {
       alert('Error: Flights must be grounded at midnight')
+    } else {
+      setRotationList(current => [...current, newFlight])
     }
   }
 
--- src/components/FlightList.js
+++ src/components/FlightList.js
@@ -8,7 +8,7 @@ function FlightList({allFlights, addToRotation, rotationList}) {
 
     let filteredFlightCards
 
-// Filters flight list so that it only shows flights that leave from the corrent airport and are at least 20 min after the previous flight after a flight is added to the rotation list
+// Filters flight list so that it only shows flights that leave from the correct airport and are at least 20 min after the previous flight after a flight is added to the rotation list
     if (rotationList.length > 0) {
         const filteredFlights= allFlights.filter(flightObj => {
             return flightObj.origin === rotationList[rotationList.length-1].destination && flightObj.departuretime-rotationList[rotationList.length-1].arrivaltime >= 1200
